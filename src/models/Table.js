e6bc2e7f9ea50cc28d0e18146558a248927eb743
jaydeaton <jayrdeaton@gmail.com>
2019-04-28 01:04:46 -0500

    improvements

--- src/models/Table.js
+++ src/models/Table.js
@@ -6,28 +6,30 @@ module.exports = class Table {
     this.rows = rows;
     this.columns = {};
     this.seperator = '|';
-
-    for (const row of this.rows) for (const key of Object.keys(row)) {
-      if (this.columns[key]) {
-        if (this.columns[key].padding < stringLength(row[key])) this.columns[key].padding = stringLength(row[key]);
-      } else {
-        this.columns[key] = {
-          key,
-          padding: stringLength(key) > stringLength(row[key]) ? stringLength(key) : stringLength(row[key]),
-          title: key
-        };
-      };
-    };
     // options
     if (!options) options = {};
     // table title
     if (options.title) this.title = options.title;
     // columns option updates object and padding
-    if (options.columns) for (const column of options.columns) if (this.columns[column.key]) {
-      const c = this.columns[column.key];
-      Object.assign(c, column);
-      if (stringLength(c.title) > c.padding) c.padding = stringLength(c.title);
-      if (c.minimum_padding && c.minimum_padding > c.padding) c.padding = c.minimum_padding;
+    if (options.columns) for (const column of options.columns) {
+      if (!column.title) column.title = column.key;
+      if (!column.padding) column.padding = stringLength(column.title);
+      if (column.minimum_padding && column.minimum_padding > column.padding) column.padding = column.minimum_padding;
+      if (!column.value) column.value = v => v;
+      this.columns[column.key] = column;
+    };
+    for (const row of this.rows) for (const key of Object.keys(row)) {
+      if (this.columns[key]) {
+        const { padding, value } = this.columns[key];
+        if (padding < stringLength(value(row[key]))) this.columns[key].padding = stringLength(value(row[key]));
+      } else {
+        this.columns[key] = {
+          key,
+          padding: stringLength(key) > stringLength(row[key]) ? stringLength(key) : stringLength(row[key]),
+          title: key,
+          value: v => v
+        };
+      };
     };
     // meta option updates padding
     if (options.meta) {
