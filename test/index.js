fd06e161c531e5c2f3b1b8760f6b630bb216d3c6
jaydeaton <jayrdeaton@gmail.com>
2019-07-10 15:05:18 -0500

    updated tabley plus tests

--- test/index.js
+++ test/index.js
@@ -5,17 +5,20 @@ const table = new Table([
   {
     columnA: '1A',
     columnB: '1B',
-    columnC: '1C'
+    columnC: '1C',
+    columnNum: 0
   },
   {
     columnA: '2A',
     columnB: '2B',
-    columnC: '2C'
+    columnC: '2C',
+    columnNum: 1
   },
   {
     columnA: '3A444',
     columnB: '3B',
-    columnC: '3C'
+    columnC: '3C',
+    columnNum: 2,
   }
 ], {
   title: 'Test',
